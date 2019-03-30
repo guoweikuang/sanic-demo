ecbeb185aa5110c4c35bd79b0de485e146e54235
guoweikuang <673411814@qq.com>
2019-03-30 22:09:39 +0800

    add comment

--- app.py
+++ app.py
@@ -11,16 +11,24 @@ from config import config
 from db import context, mako, init_db
 from views.index import index
 
+# 全局对象
+redis = None
+client = None
+
+# 初始化配置
 app = Sanic(__name__)
 app.config.from_object(config)
 app.static('/static', './static')
+
+# 扩展
 mako.init_app(app, context_processors={})
 session = Session()
-redis = None
-client = None
+
+# 蓝图
 app.blueprint(index)
 
 
+# 错误处理
 @app.exception(NotFound)
 async def ignore_404s(request, exception):
     return text("Oops, That page couldn't found.")
@@ -30,9 +38,10 @@ async def server_error_handler(request, exception) -> text:
     print(exception)
     return text("Oops, server error", status=500)
 
-# app.error_handler.add(Exception, server_error_handler)
+app.error_handler.add(Exception, server_error_handler)
 
 
+# 启动前初始化
 @app.listener('before_server_start')
 async def set_db(app, loop):
     global client
@@ -43,6 +52,7 @@ async def set_db(app, loop):
     loop.set_task_factory(context.task_factory)
 
 
+# 给 request 新增参数，这里使用 aiotask-context 来保存 redis, memcache 对象
 @app.middleware("request")
 async def setup_context(request):
     global redis
